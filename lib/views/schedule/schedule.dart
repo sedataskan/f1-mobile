6e48b27041046b97af0afcddc5bb72f9d4e8dc1c
ombayus <omerbayramcavus@gmail.com>
2023-05-28 02:28:14 +0300

    notification service

--- lib/views/schedule/schedule.dart
+++ lib/views/schedule/schedule.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../services/notification_service.dart';
+
 class ScheduleScreen extends StatefulWidget {
   const ScheduleScreen({super.key});
 
@@ -8,8 +10,31 @@ class ScheduleScreen extends StatefulWidget {
 }
 
 class _ScheduleScreenState extends State<ScheduleScreen> {
+  var notificationService = NotificationService();
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    notificationService.initNotification();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ElevatedButton(
+        onPressed: () {
+          notificationService.showNotification(
+            title: "Test",
+            body: "Test1",
+          );
+          DateTime scheduledDate = DateTime.now()
+              .add(Duration(minutes: 1)); // Örnek olarak 1 saat sonra
+          notificationService.scheduleNotification(
+            id: 1,
+            title: 'Bildirim Başlığı',
+            body: 'Bildirim İçeriği',
+            scheduledDate: scheduledDate,
+          );
+        },
+        child: Text("Send Noti"));
   }
 }
