d32ccc23302bc996cf91c018452419711167e682
ombayus <omerbayramcavus@gmail.com>
2023-06-02 23:18:19 +0300

    add schedule screen

---
+++ lib/views/schedule/models/race.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Race {
+  /// Creates a meeting class with required details.
+  Race(this.eventName, this.from, this.to, this.background, this.isAllDay);
+
+  /// Event name which is equivalent to subject property of [Appointment].
+  String eventName;
+
+  /// From which is equivalent to start time property of [Appointment].
+  DateTime from;
+
+  /// To which is equivalent to end time property of [Appointment].
+  DateTime to;
+
+  /// Background which is equivalent to color property of [Appointment].
+  Color background;
+
+  /// IsAllDay which is equivalent to isAllDay property of [Appointment].
+  bool isAllDay;
+}
--- lib/views/schedule/schedule.dart
+++ lib/views/schedule/schedule.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
 
+import '../../constants/colors.dart';
 import '../../services/notification_service.dart';
+import 'models/race.dart';
+import 'models/race_data_source.dart';
 
 class ScheduleScreen extends StatefulWidget {
   const ScheduleScreen({super.key});
@@ -18,23 +22,60 @@ class _ScheduleScreenState extends State<ScheduleScreen> {
     notificationService.initNotification();
   }
 
+  // @override
+  // Widget build(BuildContext context) {
+  //   return ElevatedButton(
+  //       onPressed: () {
+  //         notificationService.showNotification(
+  //           title: "Test",
+  //           body: "Test1",
+  //         );
+  //         DateTime scheduledDate = DateTime.now()
+  //             .add(Duration(minutes: 1)); // Örnek olarak 1 saat sonra
+  //         notificationService.scheduleNotification(
+  //           id: 1,
+  //           title: 'Bildirim Başlığı',
+  //           body: 'Bildirim İçeriği',
+  //           scheduledDate: scheduledDate,
+  //         );
+  //       },
+  //       child: Text("Send Noti"));
+  // }
+
+  List<Race> _getDataSource() {
+    final List<Race> races = <Race>[];
+    final DateTime today = DateTime.now();
+    final DateTime day1 = DateTime(today.year, today.month, today.day, 9);
+    final DateTime day2 = DateTime(today.year, today.month, today.day + 10, 9);
+    races.add(Race('Race 1', day1, day1, Color(0xFF0F8644), false));
+    races.add(
+        Race('Race 2', day2, day2, Color.fromARGB(255, 109, 161, 133), false));
+    return races;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-        onPressed: () {
-          notificationService.showNotification(
-            title: "Test",
-            body: "Test1",
-          );
-          DateTime scheduledDate = DateTime.now()
-              .add(Duration(minutes: 1)); // Örnek olarak 1 saat sonra
-          notificationService.scheduleNotification(
-            id: 1,
-            title: 'Bildirim Başlığı',
-            body: 'Bildirim İçeriği',
-            scheduledDate: scheduledDate,
-          );
-        },
-        child: Text("Send Noti"));
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Schedule', style: TextStyle(color: Colors.black)),
+          backgroundColor: AppColors.primaryColorLight,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: SfCalendar(
+            view: CalendarView.schedule,
+            dataSource: RaceDataSource(_getDataSource()),
+            // by default the month appointment display mode set as Indicator, we can
+            // change the display mode as appointment using the appointment display
+            // mode property
+            monthViewSettings: const MonthViewSettings(
+                appointmentDisplayMode:
+                    MonthAppointmentDisplayMode.appointment),
+            onTap: (CalendarTapDetails details) {
+              print(details);
+            },
+            showNavigationArrow: true,
+          ),
+        ));
   }
 }
