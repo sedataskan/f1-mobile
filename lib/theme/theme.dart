447c841bbd091aafc731c3e10522146cc3a67529
Seda <sedanurgfb6671@hotmail.com>
2023-06-03 21:15:02 +0300

    added api datas to schedule screen

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -3,6 +3,9 @@ import '../constants/colors.dart';
 
 class AppTheme {
   static ThemeData lightTheme = ThemeData.light().copyWith(
+    progressIndicatorTheme: const ProgressIndicatorThemeData(
+      color: AppColors.primaryColorLight,
+    ),
     colorScheme: const ColorScheme.light().copyWith(
       secondary: AppColors.primaryColorLight,
     ),
--- lib/views/schedule/schedule.dart
+++ lib/views/schedule/schedule.dart
@@ -6,6 +6,9 @@ import '../../services/notification_service.dart';
 import 'models/race.dart';
 import 'models/race_data_source.dart';
 
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
+
 class ScheduleScreen extends StatefulWidget {
   const ScheduleScreen({super.key});
 
@@ -15,11 +18,27 @@ class ScheduleScreen extends StatefulWidget {
 
 class _ScheduleScreenState extends State<ScheduleScreen> {
   var notificationService = NotificationService();
+
+  Future<List<dynamic>> getData() async {
+    var url = Uri.https('ergast.com', '/api/f1/current.json', {'q': '{http}'});
+
+    // Await the http get response, then decode the json-formatted response.
+    var response = await http.get(url);
+    if (response.statusCode == 200) {
+      var jsonResponse =
+          convert.jsonDecode(response.body) as Map<String, dynamic>;
+      return jsonResponse["MRData"]["RaceTable"]["Races"].toList();
+    } else {
+      print('Request failed with status: ${response.statusCode}.');
+      return [];
+    }
+  }
+
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     notificationService.initNotification();
+    getData();
   }
 
   // @override
@@ -42,33 +61,96 @@ class _ScheduleScreenState extends State<ScheduleScreen> {
   //       child: Text("Send Noti"));
   // }
 
-  List<Race> _getDataSource() {
+  List<Race> _getDataSource(List<dynamic> data) {
     final List<Race> races = <Race>[];
     final DateTime today = DateTime.now();
-    final DateTime day1 = DateTime(today.year, today.month, today.day, 9);
-    final DateTime day2 = DateTime(today.year, today.month, today.day + 10, 9);
-    races.add(Race('Race 1', day1, day1, Color(0xFF0F8644), false));
-    races.add(
-        Race('Race 2', day2, day2, Color.fromARGB(255, 109, 161, 133), false));
+
+    for (var i = 0; i < data.length; i++) {
+      final year = data[i]["date"].split("-")[0];
+      final month = data[i]["date"].split("-")[1];
+      final day = data[i]["date"].split("-")[2];
+      final time = data[i]["time"].split(":")[0];
+
+      final DateTime date = DateTime(
+          int.parse(year), int.parse(month), int.parse(day), int.parse(time));
+
+      final color = date == today
+          ? Colors.green
+          : date.isBefore(today)
+              ? Colors.red
+              : Colors.blue;
+
+      races.add(Race(data[i]["raceName"], date, date, color, false));
+    }
+
     return races;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Schedule', style: TextStyle(color: Colors.black)),
-          backgroundColor: AppColors.primaryColorLight,
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
+      appBar: AppBar(
+        title: const Text('Schedule', style: TextStyle(color: Colors.black)),
+        backgroundColor: AppColors.primaryColorLight,
+      ),
+      body: FutureBuilder(
+        future: getData(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Container(
+              color: AppColors.white,
+              child: const Center(
+                child: CircularProgressIndicator(
+                  color: AppColors.primaryColor,
+                ),
+              ),
+            );
+          } else if (snapshot.hasError) {
+            return _error();
+          } else {
+            var data = snapshot.data!;
+            return data.isEmpty ? _error() : _races(data);
+          }
+        },
+      ),
+    );
+  }
+
+  Container _races(List<dynamic> data) {
+    return Container(
+      color: AppColors.white,
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(0.0),
           child: SfCalendar(
             view: CalendarView.schedule,
-            dataSource: RaceDataSource(_getDataSource()),
+            scheduleViewSettings: ScheduleViewSettings(
+              appointmentItemHeight: 60,
+              monthHeaderSettings: MonthHeaderSettings(
+                height: 70,
+                textAlign: TextAlign.start,
+                monthTextStyle: TextStyle(
+                  fontSize: 20,
+                  color: AppColors.black,
+                ),
+                backgroundColor: AppColors.primaryColorLight,
+              ),
+            ),
+            todayHighlightColor: AppColors.primaryColor,
+            dataSource: RaceDataSource(_getDataSource(data)),
             onTap: (CalendarTapDetails details) {
               print(details.appointments?[0].getEventName());
             },
           ),
-        ));
+        ),
+      ),
+    );
+  }
+
+  _error() {
+    return Container(
+      color: AppColors.white,
+      child: Center(child: Image.asset("assets/images/error.png")),
+    );
   }
 }
